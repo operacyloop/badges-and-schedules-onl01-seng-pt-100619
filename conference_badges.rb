daca51b1e796f588bcc5bcd1ba8a71bae47e473a
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-10-15 22:50:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,20 +1,27 @@
-def batch_badge_creator(array)
-  return "Hello, #{name}! You'll be assigned to room #{room}!"
+def badge_maker(name)
+  "Hello, my name is #{name}."
 end
 
-def assign_rooms(array)
-  count = 1
-  array.each do | name | 
-  room[count] = name
-end
-    
-end
-
-def each_with_index
+def batch_badge_creator(attendees)
+  attendees.collect do |name|
+    badge_maker(name)
+  end
 
 end
 
-def printer
-  
+def assign_rooms(attendees)
+  room=0
+  attendees.collect do |name|
+    room+=1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
 end
 
+def printer(attendees)
+  batch_badge_creator(attendees).each do |value|
+    puts value
+  end
+  assign_rooms(attendees).each do |value|
+    puts value
+  end
+end
