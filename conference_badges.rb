18463d16cba22e5e0f9d4e8f537f26289ee2ccfb
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-10-15 22:07:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def batch_badge_creator(array)
+  return "Hello, #{name}! You'll be assigned to room #{room}!"
+end
+
+def assign_rooms(array)
+  count = 1
+  array.each do | name | 
+  room[count] = name
+end
+    
+end
+
+def each_with_index
+
+end
+
+def printer
+  
+end
+
